84704ccc84e740f2eb8f46f4dff50598e2813019
CODE-INAYAT <134628417+CODE-INAYAT@users.noreply.github.com>
2024-07-05 18:26:55 +0530

    Added various restrictions code for webpage

--- script/index.js
+++ script/index.js
@@ -10,3 +10,51 @@ window.addEventListener('DOMContentLoaded', function () {
         }, 500);
     }, 6300);
 });
+
+//RCR
+document.addEventListener('DOMContentLoaded', function () {
+    document.addEventListener('contextmenu', function (event) {
+        event.preventDefault();
+    });
+});
+
+//DMR win&Max
+document.addEventListener('keydown', function (e) {
+    // Disable F12 key (developer tools)
+    if (e.keyCode === 123) {
+        e.preventDefault();
+    }
+    // Disable Ctrl+Shift+I and Ctrl+Shift+J (developer tools) for Windows
+    if (e.ctrlKey && e.shiftKey && (e.keyCode === 73 || e.keyCode === 74)) {
+        e.preventDefault();
+    }
+    // Disable Command+Option+I and Command+Option+J (developer tools) for macOS
+    if (e.metaKey && e.altKey && (e.keyCode === 73 || e.keyCode === 74)) {
+        e.preventDefault();
+    }
+    // Disable Ctrl+U (view source) for Windows
+    if (e.ctrlKey && e.keyCode === 85) {
+        e.preventDefault();
+    }
+    // Disable Command+Option+U (view source) for macOS
+    if (e.metaKey && e.altKey && e.keyCode === 85) {
+        e.preventDefault();
+    }
+});
+
+//SSR
+document.addEventListener('keyup', function (e) {
+    if (e.key === 'PrintScreen') {
+        navigator.clipboard.writeText('');
+        alert('Screenshots are not allowed on this page.');
+    }
+});
+
+//Ctr+P RES. win&Mac
+document.addEventListener('keydown', function (e) {
+    // Check for Ctrl+P on Windows and Command+P on macOS
+    if ((e.ctrlKey && e.key === 'p') || (e.metaKey && e.key === 'p')) {
+        e.preventDefault();
+        alert('Printing is disabled on this page.');
+    }
+});
