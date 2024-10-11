eb58c2c4a730cec2135fbacc581c8afd46fc5db4
CODE-INAYAT <134628417+CODE-INAYAT@users.noreply.github.com>
2024-10-11 23:29:15 +0530

    Added Js on infoModal & maxlength on name, email

--- script/Authentication.js
+++ script/Authentication.js
@@ -436,13 +436,43 @@ document.addEventListener('keydown', e => {
     }
 });
 
+//Function to show infoModal
+let infoModalShown = false; // Flag to track if the modal was shown
+
+// Function to show the info modal when the email input is clicked
+function showInfoModal() {
+    const infoModal = document.getElementById("infoModal");
+    const emailInput = document.getElementById("email");
+
+    if (!infoModalShown) {
+        infoModal.style.display = "flex";
+        emailInput.disabled = true;
+        infoModalShown = true;
+    }
+}
+
+// Function to hide the info modal
+document.getElementById("iUnderstandBtn").addEventListener("click", function () {
+    const infoModal = document.getElementById("infoModal");
+    const emailInput = document.getElementById("email");
+
+    infoModal.style.display = "none";
+    emailInput.disabled = false;
+    emailInput.focus();
+});
+
+// To disable browser suggestions for the email field
+document.getElementById("email").setAttribute("autocomplete", "off");
+
 // Input formatting functions
 function convertToUpperCaseAndRemoveNumbers(input) {
     input.value = input.value.replace(/[0-9]/g, '').toUpperCase();
+    input.setAttribute("maxlength", "30");
 }
 
 function convertToLowerCaseAndRemoveSpaces(input) {
     input.value = input.value.replace(/\s/g, '').toLowerCase();
+    input.setAttribute("maxlength", "40");
 }
 
 function removeSpaces(input) {
